ec56f67e861f47ceb8a709b523ca360687daa8dc
Feng Wu <50124330+a30001784@users.noreply.github.com>
2024-02-04 14:16:53 +1100

    update config file

--- stock-tracker/app.py
+++ stock-tracker/app.py
@@ -26,7 +26,7 @@ end = datetime.today().strftime('%Y-%m-%d')
 qq_password = os.getenv("QQ_PASSWORD")   # Assuming the password is stored in an environment variable
 timestr = time.strftime("%Y-%m-%d")
 finantial_ratio_file_name = "finantial_ratio_" + timestr + ".csv"
-namelist = ['it']
+namelist = ['bank','berkshire','chip','car']
 
 
 metric = ['P/B',
