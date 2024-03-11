9d2b9fd4a4b50b9405cd925bc9ae369b2011b02e
Feng Wu <50124330+a30001784@users.noreply.github.com>
2024-03-11 17:02:35 +1100

    Add Adobe to the stock list

--- earnings_report/app.py
+++ earnings_report/app.py
@@ -14,8 +14,8 @@ import configparser
 CONFIG_FILE = './config.ini'
 config = configparser.ConfigParser()
 config.read(CONFIG_FILE)
-symbols = ['TSLA','AAPL','ABNB','AMD','ARM','AMZN',\
-           'CVNA','COIN','COST','LCID','LYFT','FROG',\
+symbols = ['TSLA','AAPL','ABNB','AMD','ARM','ADBE','AMZN',\
+           'CVNA','COIN','COST','DLTR','LCID','LYFT','FROG',\
            'MSFT','META','GOOGL','JD','NIO','NFLX','NVDA','PDD',\
            'RVN','ROKU','SNAP','SQ','SNOW','TEAM','OPEN','PANW']  # Add more symbols as needed
 qq_password = os.getenv("QQ_PASSWORD") 
